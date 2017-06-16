2879ac9fe0bf5350a950502c8545ade149f40d63
chuangcm1204 <chuangcm@tsmc.com>
2017-06-16 23:27:48 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,42 +1,40 @@
-## Put comments here that give an overall description of what your functions do
-
-## Write a short comment describing this function
-
 ### Caching the Inverse of a Matrix
-
 
 makeCacheMatrix <- function(x = matrix()) {
         inverseofX=solve(x)
+        # initializing as empty
         m<-NULL
+        # set x equal to y in the parent environment
+        # clear previous calculations
         set<<-function(y){
         x<<-y
         m<<-NULL
-
 }
-
+# grabs a created x from the parent environment
 get<-function() x
-
+# define m variable as the inverseofx function which takes the inverse of a matrix 
 setInverse<-function(inverseofX) m<<-inverseofX
-
+# get the m from the parent environment
 getInverse<-function()m
-
+# return these functions as a list to the parent environment 
 list(set=set, get=get,
     setInverse=setInverse,
     getInverse=getInverse)
 
 }
 
-## Write a short comment describing this function
+# makeCacheMatrix Must be called before cacheSolve, otherwise cacheSolve would be error
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # function calls getinverse from parent environment
         
         inverseofX<- x$getInverse()
+        # if the inverse has already been calculated, then retrieve the inverse data
         if(!is.null(inverseofX)){
-        
+        message("getting cached data")
         return(inverseofX)
 }
-
+# if inverse is not null, then calls the makeCacheMatrix to return inverse. 
 data<-x$get()
 inverseofX<-solve(data)
 x$setInverse(inverseofX)
